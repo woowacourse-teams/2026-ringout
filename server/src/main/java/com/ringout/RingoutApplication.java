0d7d4c1c02ee8f7d6feba574d61b1217c3905c70
ParkJiYeoung8297 <wldud8297@gmail.com>
2026-08-07 02:22:51 +0900

    feat: 서버 환경 구축 및 CI 작성

---
+++ server/src/main/java/com/ringout/RingoutApplication.java
@@ -0,0 +1,13 @@
+package com.ringout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingoutApplication.class, args);
+	}
+
+}
